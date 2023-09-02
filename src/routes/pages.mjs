ac4a8f45b2838df5fa5d5ada377c99925750d7d5
korexlvii <86962611+korexlvii@users.noreply.github.com>
2023-09-02 13:57:19 +0200

    Update pages.mjs

--- src/routes/pages.mjs
+++ src/routes/pages.mjs
@@ -61,7 +61,7 @@ router.get('/info', async function (req, res, next) {
     instanceIp: instanceIp, // Add this line
   };
 
-  res.render('info', { title: 'Node DemoApp: Info', info: info });
+  res.render('info', info);
 });
 
 
